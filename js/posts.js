ff90f1c404238e5cfbc65367e57e28ba11ceb9da
kounoyasei <363070454@qq.com>
2020-03-04 23:18:36 +0800

    Site updated: 2020-03-04 23:18:36

--- js/posts.js
+++ js/posts.js
@@ -1,1 +1,1 @@
-[{"title":"Hello World","date":"2020.03.04","uri":"2020/03/04/hello-world/","excerpt":""},{"title":"test","date":"2020.03.04","uri":"2020/03/04/test/","excerpt":""},{"title":"爸爸我不允许——！","date":"2020.03.04","uri":"2020/03/04/爸爸我不允许——！/","excerpt":""}]
+[{"title":"Hello World","date":"2020.03.04","uri":"2020/03/04/hello-world/","excerpt":""}]
